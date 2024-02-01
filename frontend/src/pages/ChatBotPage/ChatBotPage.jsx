5cd469f07b812f5af7dc0a148547983ea5252842
TIENDAT23-LAPTO\Tien Dat <nguyentiendat9904@gmail.com>
2024-02-01 14:17:45 +0700

    add submit and svg icon

--- frontend/src/pages/ChatBotPage/ChatBotPage.jsx
+++ frontend/src/pages/ChatBotPage/ChatBotPage.jsx
@@ -3,11 +3,11 @@ import SendMessage from "./SendMessage";
 
 function ChatBotPage() {
   return (
-    <div className="flex flex-col">
-      <div>
+    <div className="flex flex-col h-screen">
+      <div className="flex-grow">
         <Conversation />
       </div>
-      <div className="sticky bottom-0">
+      <div className="sticky bottom-0 pl-2 pr-4 pt-5">
         <SendMessage />
       </div>
     </div>
--- frontend/src/pages/ChatBotPage/SendMessage.jsx
+++ frontend/src/pages/ChatBotPage/SendMessage.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { IoSend } from "react-icons/io5";
 
 function SendMessage() {
   const [message, setMessage] = useState("");
@@ -15,32 +16,30 @@ function SendMessage() {
 
   return (
     <div className=" left-0 bg-white">
-      <div className="relative flex w-full min-h-[2em] mx-auto ">
+      <form
+        className="relative flex w-full min-h-[2em] mx-auto"
+        onSubmit={handleSend}
+      >
         <input
           type="text"
           placeholder="Write your message!"
           className="sticky flex-grow focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-12 bg-gray-200 rounded-md py-3"
           value={message}
           onChange={handleInputChange}
-        ></input>
-        <div className="flex items-center justify-center">
+        />
+        <div className="flex items-center justify-center pl-4">
           <button
             type="button"
-            className="inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-teal-500 hover:bg-blue-400 focus:outline-none"
+            className="inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-teal-500 hover:bg-teal-500 focus:outline-none"
             onClick={handleSend}
           >
-            <span className="font-bold">Send</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="h-6 w-6 ml-2 transform rotate-90"
-            >
-              <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
-            </svg>
+            <div className="flex pt-1">
+              <span className="font-bold text-xl">Send </span>
+              <IoSend className="h-6 w-6 ml-2 transform " />
+            </div>
           </button>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
