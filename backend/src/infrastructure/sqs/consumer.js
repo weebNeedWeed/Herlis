a408f9c3967a9efafe233db55d58c815cb29321d
weebNeedWeed <rivenmle@gmail.com>
2024-02-21 11:44:42 +0700

    :zap: removed unused stuffs

--- backend/src/infrastructure/sqs/consumer.js
+++ backend/src/infrastructure/sqs/consumer.js
@@ -4,7 +4,7 @@ import { sendMessage } from '../../express/server-sent-events';
 
 const handleMessage = async (message) => {
 	const body = JSON.parse(message.Body);
-	const prompt = `Với các thông tin sau, hãy viết một đoạn giới thiệu ngắn gọn về bác sĩ kèm địa chỉ và chuyên môn cho tôi (tôi bị ${body.symptoms}):
+	const prompt = `Với các thông tin sau, hãy viết một đoạn giới thiệu ngắn gọn về bác sĩ kèm địa chỉ và chuyên môn cho tớ (tớ bị ${body.symptoms}):
 Chuyên môn: ${body.doctor.speciality}
 Tên bác sĩ: ${body.doctor.name}
 Địa chỉ: ${body.doctor.address}`;
@@ -14,7 +14,9 @@ Tên bác sĩ: ${body.doctor.name}
 	});
 
 	const text = response.text();
-	sendMessage(body.conversationId, encodeURI(text));
+    const msg = `Và tớ cũng dự đoán rằng có thể cậu bị **${body.symptoms}**. Nên tớ đề xuất cho cậu một số chuyên gia, bác sĩ tâm lý để cậu có thể tìm kiếm lời khuyên, giải pháp tốt hơn:\n
+${text}`;
+	sendMessage(body.conversationId, encodeURI(msg));
 }
 
 const consumer = Consumer.create({
